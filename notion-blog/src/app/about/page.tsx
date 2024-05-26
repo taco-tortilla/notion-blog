fea2041e9c4fa61bf7e0831b95ceddb5064d75d0
taco-tortilla <tortillataco0@gmail.com>
2024-05-26 19:22:09 +0900

    feat: about page

--- notion-blog/src/app/about/page.tsx
+++ notion-blog/src/app/about/page.tsx
@@ -1,23 +1,46 @@
+import WebIcon from '@mui/icons-material/Web';
+import EmailIcon from '@mui/icons-material/Email';
+import XIcon from '@mui/icons-material/X';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Image from 'next/image';
+import ProfileImage from '../../../public/profile.jpg';
+
 export default function About() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main className="grid grid-cols-1 py-5 md:py-14">
-      <div className="flex flex-col items-center justify-center bg-white rounded-md drop-shadow-md p-20">
-        <div className="py-20">about</div>
+      <div className="flex flex-col items-center justify-center bg-white rounded-md drop-shadow-md p-6 md:p-20">
+        <div className="py-5 md:py-10">
+          <Image
+            src={ProfileImage}
+            width={150}
+            height={150}
+            alt="Picture of profile"
+            className="rounded-full"
+          />
+        </div>
         <div className="py-10">
-          <p className="text-lg font-bold text-center pb-4">
-            Taisei Hasegawa (tortilla)
+          <p className="text-xl font-bold text-center pb-6">
+            Taisei Hasegawa <br></br>
+            (tortilla)
           </p>
-          <p className="py-2">
-            web: <a href="https://tortilla.page">tortilla.page</a>
+          <p className="text-center text-lg pb-10">
+            Thanks for comming my blog ^^
           </p>
-          <p className="py-2">sns: </p>
-          <p className="py-2">
-            contact:
-            <a href="mailto:tortillataco00@gmail.com">
-              tacotortilla0@gmail.com
+          <div className="flex items-center justify-between">
+            <a href="https://tortilla.page">
+              <WebIcon fontSize="large" />
             </a>
-          </p>
+            <a href="https://x.com/tortillataco0">
+              <XIcon fontSize="large" />
+            </a>
+            <a href="mailto:tortillataco00@gmail.com">
+              <GitHubIcon fontSize="large" />
+            </a>
+            <a href="mailto:tortillataco00@gmail.com">
+              <EmailIcon fontSize="large" />
+            </a>
+          </div>
         </div>
       </div>
     </main>
--- notion-blog/src/app/components/common/Header.tsx
+++ notion-blog/src/app/components/common/Header.tsx
@@ -3,8 +3,10 @@ import Link from 'next/link';
 export function Header() {
   return (
     <div className="flex py-5">
-      <Link href="/">tortilla blog</Link>
-      <Link href="/about" className="ml-auto">
+      <Link href="/" className="text-lg font-lg">
+        tortilla blog
+      </Link>
+      <Link href="/about" className="ml-auto text-lg font-lg">
         About tortiila
       </Link>
     </div>
